68e1875288b01f7f980865fa20f5de6ba68ab06d
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2015-01-29 10:08:17 +0100

    Brach

--- src/exemploherdanza1/pkg1/ExemploHerdanza11.java
+++ src/exemploherdanza1/pkg1/ExemploHerdanza11.java
@@ -10,12 +10,12 @@ public class ExemploHerdanza11 {
     public static void main(String[] args)
     {
         EmpregadoTemporal emT= new EmpregadoTemporal("aa",1000f);
-        EmpregadoFixo emF= new EmpregadoFixo("bb",1050f,30);
+        EmpregadoFixo emF= new EmpregadoFixo("bb",2000f,30);
         Directivo dir= new Directivo("cc",2000f,350f);
-        Traballador traballador =new EmpregadoFixo("tt",3990f,40);
+        Traballador traballador =new EmpregadoFixo("tt",3990f,30);
         System.out.println(emT.visualizarDato());
         System.out.println(emF.visualizarDato());
-        System.out.println("Colaboracion de Seijas");
+        System.out.println("Hiper meha Colaboracion de Seijas");
         System.out.println(dir.visualizarDato());
         System.out.println(traballador.visualizarDato());
     }
