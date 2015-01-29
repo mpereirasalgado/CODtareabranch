d7755b427dc710fb3a7cbd5c0f4832d2557c3f97
micaelr <micaelr@micaelr-PC>
2015-01-29 09:50:34 +0100

    commit trabajo cod

---
+++ src/exemploherdanza1/pkg1/ExemploHerdanza11.java
@@ -0,0 +1,22 @@
+/**
+ *tarea cod ramas 
+ */
+ 
+package exemploherdanza1.pkg1;
+
+/**@author Administrador*/
+public class ExemploHerdanza11 {
+
+    public static void main(String[] args)
+    {
+        EmpregadoTemporal emT= new EmpregadoTemporal("aa",1000f);
+        EmpregadoFixo emF= new EmpregadoFixo("bb",1000f,30);
+        Directivo dir= new Directivo("cc",2000f,350f);
+        Traballador traballador =new EmpregadoFixo("tt",3000f,40);
+        System.out.println(emT.visualizarDato());
+        System.out.println(emF.visualizarDato());
+        System.out.println(dir.visualizarDato());
+        System.out.println(traballador.visualizarDato());
+    }
+    
+}
